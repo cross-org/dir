1eedd86b9879f430b2865edb057dcce5bd7ed155
Pinta365 <hello@pinta.land>
2024-03-23 20:37:11 +0100

    docs

--- src/dir.ts
+++ src/dir.ts
@@ -8,6 +8,7 @@ export { DirectoryTypes } from "./config.ts";
  * Retrieves the path to a standard user directory based on the provided string and the current operating system.
  *
  * @param {string} type - The type of directory to retrieve as a string.
+ * @param {boolean} parseWindowsSpecialDirectories - Optional boolean to resolve Windows special folders with Powershell.
  * @returns {Promise<string>} A promise that resolves to the full path of the directory.
  */
 export async function dir(type: keyof typeof DirectoryTypes, parseWindowsSpecialDirectories?: boolean): Promise<string>;
@@ -16,6 +17,7 @@ export async function dir(type: keyof typeof DirectoryTypes, parseWindowsSpecial
  * Retrieves the path to a standard user directory based on the provided type and the current operating system.
  *
  * @param {DirectoryTypes} type - The type of directory to retrieve.
+ * @param {boolean} parseWindowsSpecialDirectories - Optional boolean to resolve Windows special folders with Powershell.
  * @returns {Promise<string>} A promise that resolves to the full path of the directory.
  */
 export async function dir(type: DirectoryTypes, parseWindowsSpecialDirectories?: boolean): Promise<string>;
@@ -24,6 +26,7 @@ export async function dir(type: DirectoryTypes, parseWindowsSpecialDirectories?:
  * Retrieves the path to a standard user directory based on the provided type and the current operating system.
  *
  * @param {DirectoryTypes} type - The type of directory to retrieve (e.g., 'home', 'cache', 'config').
+ * @param {boolean} parseWindowsSpecialDirectories - Optional boolean to resolve Windows special folders with Powershell.
  * @returns {Promise<string>} A promise that resolves to the full path of the directory.
  * @throws {Error} If the directory type is not supported on the current platform.
  * @throws {Error} If no suitable environment variable is found for the requested directory.
